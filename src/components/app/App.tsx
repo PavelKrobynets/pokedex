a254114e2066b85654ba2ebc99505ecb407f0236
Pavel <krobynetspavel@gmail.com>
2024-09-05 17:12:47 +0300

    starter

---
+++ src/components/app/App.tsx
@@ -0,0 +1,12 @@
+import Pokedex from '../../pages/Pokedex'
+import './App.scss'
+
+function App() {
+  return (
+    <div className='app'>
+      <Pokedex/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/pages/Pokedex.tsx
@@ -0,0 +1,9 @@
+import "./pokedex.scss"
+
+export default function Pokedex() { 
+	return (
+		<div className="pokedex">
+			<h1>Hello</h1>
+		</div>
+	);
+}
